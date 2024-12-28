300804e01f6ef053da357ae915dd2e5ef2ebea1e
cauakse <caua_pd2019@hotmail.com>
2024-12-28 12:24:03 -0300

    vercel vai se fuder

--- src/index.js
+++ src/index.js
@@ -13,14 +13,8 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', "ejs");
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}))
-
-app.use(express.static(path.join(__dirname, 'public'), {
-  setHeaders: (res, path, stat) => {
-    console.log(`Servindo arquivo estático: ${path}`);
-  }
-}));
 
 
 app.use(router);
