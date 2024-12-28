2b19d9739dd8d2c95fb8e2a52c05c62cc0d6e9e2
cauakse <caua_pd2019@hotmail.com>
2024-12-28 11:31:15 -0300

    vercel

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import express from 'express'
 import path from 'node:path'
 import router from './routes.js'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', "ejs");
-app.set('views', path.join(process.argv[1].replace("index.js",""),'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}))
